000850498b8c4d6f45ea8dbaa39fc72fa4d8c96e
linusyoung <linusyoungrice@gmail.com>
2020-06-25 20:15:05 +1000

    add barrier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:fivehundreds/widgets/matchinfo.dart';
+import 'package:fivehundreds/pages/pages.dart';
+import 'package:fivehundreds/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -48,8 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(
@@ -73,116 +72,16 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _createNewMatch(context),
+        // onPressed: () => _createNewMatch(context),
+        onPressed: _showMatchPage,
         child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-}
 
-class NewMatch extends StatefulWidget {
-  @override
-  _NewMatchState createState() => _NewMatchState();
-}
-
-class _NewMatchState extends State<NewMatch> {
-  List<bool> _isSelected = [true, false, false];
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-      child: Container(
-        height: 300,
-        width: 200,
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: 'Team 1'),
-                  ),
-                ),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: 'Team 2'),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Total Rounds'),
-              ],
-            ),
-            ToggleButtons(
-              children: <Widget>[
-                Text('1'),
-                Text('3'),
-                Text('5'),
-              ],
-              onPressed: (int index) {
-                setState(() {
-                  for (int buttonIndex = 0;
-                      buttonIndex < _isSelected.length;
-                      buttonIndex++) {
-                    if (buttonIndex == index) {
-                      _isSelected[buttonIndex] = true;
-                    } else {
-                      _isSelected[buttonIndex] = false;
-                    }
-                  }
-                });
-              },
-              isSelected: _isSelected,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Match styles'),
-              ],
-            ),
-            ToggleButtons(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Perfect'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Avondale'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Original'),
-                ),
-              ],
-              onPressed: (int index) {
-                setState(() {
-                  for (int buttonIndex = 0;
-                      buttonIndex < _isSelected.length;
-                      buttonIndex++) {
-                    if (buttonIndex == index) {
-                      _isSelected[buttonIndex] = true;
-                    } else {
-                      _isSelected[buttonIndex] = false;
-                    }
-                  }
-                });
-              },
-              isSelected: _isSelected,
-            ),
-            RaisedButton(
-              child: Text('Create Match'),
-              color: Theme.of(context).secondaryHeaderColor,
-              onPressed: () => {Navigator.of(context).pop()},
-            )
-          ],
-        ),
-      ),
-    );
+  void _showMatchPage() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) => MatchPage()));
   }
 }
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export './match_info.dart';
+export './new_match.dart';
+export './card_icon.dart';
