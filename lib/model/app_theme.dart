e16116451ce7483a8b723d9e693fce2f40889ad8
linusyoung <linusyoungrice@gmail.com>
2020-06-29 21:55:35 +1000

    update text theme

--- lib/model/app_theme.dart
+++ lib/model/app_theme.dart
@@ -3,20 +3,19 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   AppTheme._();
-  final textTheme = GoogleFonts.caveatTextTheme();
 
   static final ThemeData lightTheme = ThemeData(
     primarySwatch: Colors.teal,
     visualDensity: VisualDensity.adaptivePlatformDensity,
-    textTheme: GoogleFonts.caveatTextTheme().copyWith(
-      headline4: GoogleFonts.caveatTextTheme().headline4.copyWith(
+    textTheme: GoogleFonts.courgetteTextTheme().copyWith(
+      // button text
+
+      subtitle2: GoogleFonts.courgetteTextTheme().subtitle2.copyWith(
             color: Colors.white,
           ),
-      headline5: GoogleFonts.caveatTextTheme().headline5.copyWith(
-            fontWeight: FontWeight.bold,
-          ),
-      headline6: GoogleFonts.caveatTextTheme().headline6.copyWith(
-            fontWeight: FontWeight.bold,
+      // app bar text
+      headline4: GoogleFonts.courgetteTextTheme().headline4.copyWith(
+            color: Colors.white,
           ),
     ),
   );
